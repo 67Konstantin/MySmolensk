186539fcbd05fdea422923a258c53e1d3be08d5f
67Konstantin <113464840+67Konstantin@users.noreply.github.com>
2023-03-31 22:16:30 +0300

    Commit

--- app/src/main/java/com/example/mysmolensk/ListRoutes.java
+++ app/src/main/java/com/example/mysmolensk/ListRoutes.java
@@ -1,8 +1,17 @@
 package com.example.mysmolensk;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 public class ListRoutes extends AppCompatActivity {
 
--- app/src/main/java/com/example/mysmolensk/MainActivity.java
+++ app/src/main/java/com/example/mysmolensk/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.mysmolensk;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
+import ru.rustore.geo.MapGlobalConfig;
+import ru.rustore.geo.MapViewConfig;
+import ru.rustore.geo.data.CompassLocationMode;
+import ru.rustore.geo.data.MapLocation;
+import ru.rustore.geo.data.MapStyle;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapViewConfig mapViewConfig = new MapViewConfig("RS63c048bc59728494f6670deda9de27a98edb1da70cfab0aa907ebc0ee9b388",new MapLocation(54.7818,32.0401,1f,1f,1f,1.0,1.0), 1.0f,
+                MapStyle.Light.INSTANCE, CompassLocationMode.FreeArrow);
+        MapGlobalConfig.Companion.setMapGlobalConfig(mapViewConfig);
     }
 }
+
